35fb3e1738a1b28d3a422f3c7f16488d6aa7e1f2
softech.series <renesalinas0525@gmail.com>
2022-05-17 14:51:44 -0500

    promesa

---
+++ Parcial 3/Promise/Promise.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#btnCambiarHeaderP').click(function(){
+    
+        let promesa = new Promise(function(resolve,reject){
+        var solicitud = new XMLHttpRequest();
+        solicitud.onreadystatechange = function(){
+            if (solicitud.readyState == 4 && solicitud.status == 200){
+                resolve(solicitud.responseText);
+            }};
+            solicitud.open("GET","getHeader.txt",true);
+            solicitud.send();
+        
+        });
+    
+        promesa.then(function(value){document.getElementById("contenido").innerHTML = value;});
+    
+        
+    });
+})
